d52842c892ce00e160979db09cc0d6900ca780b6
Ruben Aguilera <admin@example.com>
2019-04-27 23:45:16 +0200

    Fix airport url schemas http

--- src/main/java/com/autentia/airlines/rest/AirportController.java
+++ src/main/java/com/autentia/airlines/rest/AirportController.java
@@ -1,15 +1,14 @@
 package com.autentia.airlines.rest;
 
+import com.autentia.airlines.model.Airport;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-
-import com.autentia.airlines.model.Airport;
 
 @RestController
 @RefreshScope
@@ -27,7 +26,7 @@ public class AirportController {
 	@GetMapping("/airports")
 	@HystrixCommand(fallbackMethod="getAirportsEmpty")
 	public Airport[] getAirports() {
-		return restTemplate.getForObject("http://" + this.airportsUrl + "/airport", Airport[].class);
+		return restTemplate.getForObject(this.airportsUrl + "/airport", Airport[].class);
 			
 	}
 	
